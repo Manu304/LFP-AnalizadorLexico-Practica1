6d5ce92afba1fefe846c2c1c6ebe43d91dde8638
Manuel Rojas <manuerjs70@gmail.com>
2021-09-24 08:28:26 -0600

    Se agrego clase con metodo main

---
+++ src/principal/Principal.java
@@ -0,0 +1,13 @@
+package principal;
+
+import user_interface.VentanaPrincipal;
+
+/**
+ *
+ * @author Manu
+ */
+public class Principal {
+    public static void main(String[] args) {
+        VentanaPrincipal p = new VentanaPrincipal();
+    }
+}
