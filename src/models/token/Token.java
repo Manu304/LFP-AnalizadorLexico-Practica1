de5abda3d3d7e9d220bd41f06fdf0286c79f96b2
Manuel Rojas <manuerjs70@gmail.com>
2021-10-03 02:13:33 -0600

    Elimine parametro posicion inicial y final de la clase Token

--- src/models/token/Token.java
+++ src/models/token/Token.java
@@ -10,128 +10,62 @@ public class Token {
     private String cadena;
     private TipoToken tipo;
     private int fila;
-    private int columIni;
-    private int columFin;
     private int cantidad;
+    private int columna;
 
-    /**
-     * Constructor del objeto Token
-     * 
-     * @param cadena   valor que almacenará el toquen a crear
-     * @param tipo     Tipo de token que se va a crear
-     * @param columIni Posición inicial del token en la fila
-     * @param columFin Posición final del token en la fila
-     */
-
-    public Token(String cadena, TipoToken tipo, int columIni, int columFin) {
+    public Token(String cadena, TipoToken tipo) {
         this.cadena = cadena;
         this.tipo = tipo;
-        this.columIni = columIni;
-        this.columFin = columFin;
         this.cantidad = 1;
     }
 
-    /**
-     * Constructor del objeto Token
-     * 
-     * @param cadena   valor que almacenará el toquen a crear
-     * @param tipo     Tipo de token que se va a crear
-     * @param fila     Fila donde se encuentra el token a crear
-     * @param columIni Posición inicial del token en la fila
-     * @param columFin Posición final del token en la fila
-     */
-
-    public Token(String cadena, TipoToken tipo, int fila, int columIni, int columFin) {
-        this(cadena, tipo, columIni, columFin);
+    public Token(String cadena, TipoToken tipo, int fila, int columna) {
+        this(cadena, tipo);
+        this.columna = columna;
         this.fila = fila;
     }
 
     /**
      * Método para obtener el tipo de token
-     * 
+     *
      * @return Retornar el tipo del token, si no lo tiene asignado retorna null.
      */
-
     public TipoToken getTipo() {
         return tipo;
     }
 
     /**
      * Método para obtener el valor de la cadena del token
-     * 
+     *
      * @return Retorna un string con el contenido del token
      */
-
     public String getCadena() {
         return cadena;
     }
 
     /**
      * Método para obtener la fila en la que se encuentra el token
-     * 
+     *
      * @return Retorna el número de fila en la que está el token.
      */
-
     public int getFila() {
         return fila;
     }
 
     /**
      * Método para cambmiar el número de fila donde se encuentra el token
-     * 
+     *
      * @param fila Número de fila en la que se encuentra el token
      */
-
     public void setFila(int fila) {
         this.fila = fila;
     }
 
     /**
-     * Método para obtener la columna en la que inicia el token
-     * 
-     * @return Retorna el número de columna en la que inicia el token.
-     */
-
-    public int getColumIni() {
-        return columIni;
-    }
-
-    /**
-     * Método para cambmiar el número de columna donde inicia el token
-     * 
-     * @param columIni Número de columna en la que inicia el token
-     */
-
-    public void setColumIni(int columIni) {
-        this.columIni = columIni;
-    }
-
-    /**
-     * Método para obtener la columna en la que termina el token
-     * 
-     * @return Retorna el número de columna en la que termina el token.
-     */
-
-    public int getColumFin() {
-        return columFin;
-    }
-
-    /**
-     * Método para cambmiar el número de columna donde termina el token
-     * 
-     * @param columFin Número de columna en la que termina el token
-     */
-
-    public void setColumFin(int columFin) {
-        this.columFin = columFin;
-    }
-
-    /**
      * Método para obtener la cantidad de veces que se repite el token
-     * 
+     *
      * @return Valor entero que define cuantas veces se repite un token.
      */
-
     public int getCantidad() {
         return cantidad;
     }
@@ -140,9 +74,16 @@ public class Token {
      * Método para aumentar la cantidad de veces que se repite un toquen de unidad
      * en unidad.
      */
-
     public void setCantidad() {
         this.cantidad++;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+
+    public void setColumna(int columna) {
+        this.columna = columna;
     }
 
     @Override
@@ -156,25 +97,31 @@ public class Token {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
+        if (this == obj) {
             return true;
-        if (obj == null)
+        }
+        if (obj == null) {
             return false;
-        if (getClass() != obj.getClass())
+        }
+        if (getClass() != obj.getClass()) {
             return false;
+        }
         Token other = (Token) obj;
         if (cadena == null) {
-            if (other.cadena != null)
+            if (other.cadena != null) {
                 return false;
-        } else if (!cadena.equals(other.cadena))
+            }
+        } else if (!cadena.equals(other.cadena)) {
             return false;
-        if (tipo != other.tipo)
+        }
+        if (tipo != other.tipo) {
             return false;
+        }
         return true;
     }
 
     @Override
     public String toString() {
-        return "Token [cadena=" + cadena + " cantidad=" + cantidad +" tipo=" + tipo +"]";
+        return "Token [cadena=" + cadena + " cantidad=" + cantidad + " tipo=" + tipo + "]";
     }
 }
