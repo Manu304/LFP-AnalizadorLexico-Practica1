c388da27d9365dbc4b4887fc75c51e20898eaf91
Manuel Rojas <manuerjs70@gmail.com>
2021-10-04 13:43:49 -0600

    Se arreglaron errores para el metodo de buscar palabras

--- src/controllers/ManejadorVentanaBuscar.java
+++ src/controllers/ManejadorVentanaBuscar.java
@@ -1,5 +1,10 @@
 package controllers;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
 import javax.swing.text.JTextComponent;
 import user_interface.VentanaBuscar;
@@ -11,13 +16,15 @@ import user_interface.VentanaBuscar;
 public class ManejadorVentanaBuscar {
 
     private VentanaBuscar frame;
-    //private String textActual;
-    //private int coincidenciaTotal;
-    //private int coincidenciaActual;
+    private String textActual;
+    private int coincidenciaTotal;
+    private int coincidenciaActual;
+    private boolean caseSensitive;
 
     public ManejadorVentanaBuscar(VentanaBuscar frame) {
         this.frame = frame;
-        //coincidenciaActual = 0;
+        coincidenciaActual = 0;
+        caseSensitive = frame.getjCheckBox1().isSelected();
     }
 
     public void activarBotones() {
@@ -25,69 +32,69 @@ public class ManejadorVentanaBuscar {
         if (frame.getTextBuscar().getText() != null && !frame.getTextBuscar().getText().isBlank()) {
             activar = true;
         }
-        frame.getBotonAnterior().setEnabled(activar);
         frame.getBotonSiguiente().setEnabled(activar);
+        frame.getBotonAnterior().setEnabled(activar);
     }
-/*
-    private int getCoincidencias(String textBuscar, String textTotal) {
-        int posicion = 0;
-        int contador = 0;
-        while (posicion > -1) {
-            /*if (posicion > 0 && (textTotal.charAt(posicion - 1) == '\n')) {
-                posicion--;
-                System.out.println("estoy en el while");
 
-            }*/
-            /*
-            posicion = textTotal.indexOf(textBuscar, posicion + textBuscar.length());
-            contador++;
+    public void buscarPalabra(JTextComponent componente, String cadena, boolean caseSensitive, boolean buscarSiguiente) {
+        this.caseSensitive = caseSensitive; //VERIFICAR SI AUN SE PUEDE UTILIZAR
+        String textoArea;
+        int posicion = componente.getCaretPosition(), textoAreaTamanio = 0;
+        int encontrado;
+        try {
+            textoAreaTamanio = componente.getDocument().getLength();
+            textoArea = componente.getDocument().getText(0, textoAreaTamanio);
+        } catch (BadLocationException ex) {
+            ex.printStackTrace();
+            textoArea = "";
         }
-        System.out.println("contador : " + contador);
-        return contador;
+
+        if (!caseSensitive) {
+            textoArea = textoArea.toLowerCase();
+            cadena = cadena.toLowerCase();
+        }
+
+        int ultimoResultado = buscarSiguiente ? textoArea.lastIndexOf(cadena) : textoArea.indexOf(cadena);
+        boolean condicionBuscar = buscarSiguiente ? posicion >= textoAreaTamanio : false;
+
+        if (condicionBuscar || (posicion - cadena.length()) == ultimoResultado) {
+            posicion = buscarSiguiente ? 0 : textoAreaTamanio + 2;
+            System.out.println("he encontrado el ultimo, ahora posicion = " + posicion);
+        }
+        if (buscarSiguiente) {
+            encontrado = textoArea.indexOf(cadena, posicion - 1);
+        } else {
+            encontrado = textoArea.lastIndexOf(cadena, posicion - cadena.length() - 1);
+        }
+
+        if (encontrado > -1) {
+            componente.setSelectionStart(encontrado);
+            componente.setSelectionEnd(encontrado + cadena.length());
+            componente.getCaret().setSelectionVisible(true);
+            cambiarLabel(textoArea, cadena);
+        } else {
+            JOptionPane.showMessageDialog(null, "No se han encontrado coincidencias", "No encontrado", JOptionPane.INFORMATION_MESSAGE);
+        }
+        System.out.println("tamañio: " + textoAreaTamanio + " posicion: " + posicion + " encontrado: " + encontrado);
     }
 
-    private void cambiarLabelBusqueda(String textBuscar) {
-        if (textActual.equals(textBuscar)) {
-            coincidenciaActual++;
-        } else {
-            coincidenciaActual = 0;
-            coincidenciaTotal = 0;
+    public void cambiarLabel(String textoTotal, String cadena) {
+        if (textActual == null || !cadena.equals(textActual) || caseSensitive != frame.getjCheckBox1().isSelected()) {
+            caseSensitive = frame.getjCheckBox1().isSelected();
+            textActual = cadena;
+            int posicion = 0, contador = 0;
+            while (posicion > -1 && posicion + cadena.length() <= textoTotal.length()) {
+                posicion = textoTotal.indexOf(cadena, posicion + cadena.length());
+                contador++;
+            }
+            coincidenciaTotal = contador;
+        }
+        coincidenciaActual++;
+        if (coincidenciaActual > coincidenciaTotal) {
+            coincidenciaActual = 1;
         }
         frame.getLabelBusqueda().setText("Coincidencia " + coincidenciaActual + " de " + coincidenciaTotal);
 
-    }
-    */
-
-    public void highLight(JTextComponent component, String patteren) {
-        try {
-            Document doc = component.getDocument();
-            //String text = component.getText(0, doc.getLength());
-            int pos = component.getCaretPosition();
-            boolean found = false;
-            int findLength = patteren.length();
-            // Rest the search position if we're at the end of the document
-            if (pos + findLength > doc.getLength()) {
-                pos = 0;
-            }
-            while (pos + findLength <= doc.getLength()) {
-                // Extract the text from teh docuemnt
-                String match = doc.getText(pos, findLength).toLowerCase();
-                // Check to see if it matches or request
-                if (match.equals(patteren)) {
-                    found = true;
-                    break;
-                }
-                pos++;
-            }
-
-            if (found) {
-                component.setSelectionStart(pos);
-                component.setSelectionEnd(pos + patteren.length());
-                component.getCaret().setSelectionVisible(true);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
 }
