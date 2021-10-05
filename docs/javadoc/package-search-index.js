35eee47ed1a1bde5f303af664e8e951bec630b9f
Manuel Rojas <manuerjs70@gmail.com>
2021-10-05 11:14:19 -0600

    Se agrego javadoc

---
+++ docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"archives"},{"l":"controllers"},{"l":"models.token"},{"l":"principal"},{"l":"services"},{"l":"user_interface"},{"l":"user_interface.reports"},{"l":"util"}];updateSearchResults();
